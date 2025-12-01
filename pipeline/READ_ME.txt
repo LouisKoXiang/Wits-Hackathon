e863ba1cd00e9a7ae5ef3eaed6a91e9ff09280ce
WesleyHuang <huangwisley@gmail.com>
2025-12-01 21:12:58 +0800

    特徵工程pipeline版
    1. 此次改版將特徵工程轉成pipeline
    2. 因為是用XGBoost, 已沒有使用 Scaler

---
+++ pipeline/READ_ME.txt
@@ -0,0 +1,10 @@
+Python3_12_12_requirements.txt : 環境設定檔
+
+models.zip : 所有需要的 pkl, json
+
+realTestData.csv : 測資
+
+lending_predictor.py : pipeline 定義檔
+
+sample_main.py : 呼叫範例
+
---
+++ pipeline/sample_main.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import os
+import joblib
+from loan_predictor import LoanPredictor 
+
+if __name__ == "__main__":
+    
+    # 1. 初始化 predictor, 參數 custom_paths 可自訂義檔案來源路徑, 請參考 loan_predictor.py
+    predictor = LoanPredictor()
+    
+    # 2. 準備測資, 丟棄目標標籤欄位(如果有的話)
+    test_data = pd.read_csv(predictor.paths['test_data'])
+    all_sample_data = test_data.drop(columns=['loan_repaid', 'loan_status'], errors='ignore')
+
+    # 3. 載入特徵工程 pipeline 
+    predictor = predictor.load_pipeline(predictor.paths["pipeline_pkl"])
+       
+    # 4. 預測結果
+    # ** 直接呼叫 predictor.predict 即可
+    # ** 我這裡用 generate_report, 裡面也是呼叫 predict
+    results = predictor.generate_report(all_sample_data, save_to_gcs=False)
+         
+    # 5. 輸出違約/還款的數量分佈
+    print(f"\n>> 載入驗證成功！共對 {len(all_sample_data)} 筆資料進行了預測")
+    print("\n>> 預測結果 DataFrame：")
+    print(results[['Predicted_Probability', 'Predicted_Status']])
